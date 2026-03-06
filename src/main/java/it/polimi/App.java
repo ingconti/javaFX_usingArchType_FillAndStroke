b0de8e4fc90d3f06a2d92a3ce5f54255f636957d
ing.conti <ingconti@ingconti.com>
2026-03-06 16:07:07 +0100

    added supporto for JDK 25 and files

--- src/main/java/it/polimi/App.java
+++ src/main/java/it/polimi/App.java
@@ -28,10 +28,11 @@ public class App extends Application {
     }
 
     private void drawShapes(GraphicsContext gc) {
+        int x_offset = 0;
         gc.setFill(Color.GREEN);
         gc.setStroke(Color.BLUE);
         gc.setLineWidth(5);
-        gc.strokeLine(40, 10, 10, 40);
+        gc.strokeLine(40, 10, 100, 40);
         gc.fillOval(10, 60, 30, 30);
         gc.strokeOval(60, 60, 30, 30);
         gc.fillRoundRect(110, 60, 30, 30, 10, 10);
